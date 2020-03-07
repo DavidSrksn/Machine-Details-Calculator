a9c2839458fcefea4c0dcf2af15e88ffae5edc30
DavidSrksn <s.david99@mail.ru>
2020-03-07 19:17:21 +0300

    Initial Commit

---
+++ Machine Details/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Machine Details
+//
+//  Created by David Sarkisyan on 07.03.2020.
+//  Copyright © 2020 DavidS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
